3e9658b49a26f0428176b94f5bfcc1f8e3eb2eff
Kim Lehtinen <kim@kimlehtinen.com>
2024-09-22 17:29:18 +0300

    More test cases for nestedrecursion

--- tests/recursion/nestedrecursion/nestedrecursion_test.cpp
+++ tests/recursion/nestedrecursion/nestedrecursion_test.cpp
@@ -1,10 +1,18 @@
 #include <gtest/gtest.h>
 #include "recursion/nestedrecursion/nestedrecursion.h"
 
-TEST(NestedRecursionTest, BaseCase) {
+TEST(NestedRecursionTest, BelowThreshold) {
+    EXPECT_EQ(91, nestedRecursion(0));
+    EXPECT_EQ(91, nestedRecursion(20));
     EXPECT_EQ(91, nestedRecursion(95));
+    EXPECT_EQ(91, nestedRecursion(99));
+}
+
+TEST(NestedRecursionTest, AtThreshold) {
+    EXPECT_EQ(91, nestedRecursion(100));
 }
 
 TEST(NestedRecursionTest, AboveThreshold) {
     EXPECT_EQ(100, nestedRecursion(110));
+    EXPECT_EQ(110, nestedRecursion(120));
 }
